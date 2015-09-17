7ed93c3424084ff20481717219bf8e188899b63c
Bryan Alexander <bryanalexander@me.com>
2015-09-16 16:15:36 -1000

    initial commit with test modules downloaded & test files created

---
+++ js/shopping_list_test.js
@@ -0,0 +1,13 @@
+var expect = chai.expect;
+var should = chai.should();
+
+describe('ShoppingListItem', function() {
+
+  it('should be a function', function() {
+
+    ShoppingListItem.should.be.a('function');
+
+  });
+
+});
+
