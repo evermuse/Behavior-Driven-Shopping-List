de2e6a00deeb26e0fb7a8c38eb1478a68385dd7e
Bryan Alexander <bryanalexander@me.com>
2015-09-17 13:35:42 -1000

    completed ShoppingListItem class & refactored tests slightly

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -1,4 +1,26 @@
 function ShoppingListItem(name, description) {
 
+  this.name = name;
+  this.description = description;
+  this.isDone = false;
+
 }
+
+ShoppingListItem.prototype.check = function() {
+
+  this.isDone = true;
+
+};
+
+ShoppingListItem.prototype.uncheck = function() {
+
+  this.isDone = false;
+
+};
+
+ShoppingListItem.prototype.render = function() {
+
+  return '<li class="completedIsDone"><span>[name]</span> <span>[description]</span></li>. example: <li class="completed_false"><span>name</span> <span>description</span></li>';
+
+};
 
--- js/shopping_list_test.js
+++ js/shopping_list_test.js
@@ -1,15 +1,15 @@
 var expect = chai.expect;
 var should = chai.should();
 
+var newShoppingListItem;
+
+before(function() {
+
+  newShoppingListItem = new ShoppingListItem('buy milk', 'whole milk or bust');
+
+});
+
 describe('ShoppingListItem', function() {
-
-  var newShoppingListItem;
-
-  // beforeEach(function() {
-
-  //   newShoppingListItem = new ShoppingListItem('buy milk', 'whole milk or bust');
-
-  // });
 
   it('should be a function', function() {
 
@@ -32,45 +32,64 @@ describe('ShoppingListItem', function() {
 
   });
 
+});
+
+//end ShoppingListItem property specs
+
+describe('.check', function() {
+
+
   it('should have a method named check', function() {
 
-    newShoppingListItem.should.have.method('check');
+    expect(newShoppingListItem.check).to.be.a('function');
 
   });
 
   it('calling check should set isDone to true', function() {
 
+    newShoppingListItem.check();
     newShoppingListItem.isDone.should.equal(true);
-
-  });
-
-  it('should have a method named uncheck', function() {
-
-    newShoppingListItem.should.have.method('uncheck');
-
-  });
-
-  it('calling uncheck should set isDone to false', function() {
-
-    newShoppingListItem.isDone.should.equal(false);
-
-  });
-
-  it('should have a method named render', function() {
-
-    newShoppingListItem.should.have.method('render');
-
-  });
-
-  it('calling render should construct and return an html formatted string', function() {
-
-    newShoppingListItem.render.should.return.typeof('string'); // invoke
 
   });
 
 });
 
-//end ShoppingListItem specs
+describe('.uncheck', function() {
+
+  it('should have a method named uncheck', function() {
+
+    expect(newShoppingListItem.uncheck).to.be.a('function');
+
+  });
+
+  it('calling uncheck should set isDone to false', function() {
+
+    newShoppingListItem.uncheck();
+    newShoppingListItem.isDone.should.equal(false);
+
+  });
+
+});
+
+describe('.render', function() {
+
+  it('should have a method named render', function() {
+
+    expect(newShoppingListItem.render).to.be.a('function');
+
+  });
+
+  it('calling render should construct and return an html formatted string with <li> element', function() {
+
+    newShoppingListItem.render();
+    newShoppingListItem.render().should.be.a('string');
+    expect(newShoppingListItem.render()).to.match(/<li/);
+
+  });
+
+});
+
+//end ShoppingListItemm method specs
 
 describe('ShoppingList', function() {
 
