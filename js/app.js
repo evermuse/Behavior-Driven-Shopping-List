48105f147b15466c58307a14f4464a76a578b5b9
Bryan Alexander <bryanalexander@me.com>
2015-09-18 13:32:06 -1000

    created app.js & added function calls

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
-var submitLink = document.querySelector('#add_shopping_list_item_button');
+theShoppingList = new ShoppingList();
+theShoppingList.render();
+
+document.querySelector('#add_shopping_list_item_button').addEventListener('click', add_to_shopping_list());
 
 submitLink.addEventListener('click', function() {
 
   console.log('clickworked');
 
 });
+
+function add_to_shopping_list() {
+
+
+}
 
 function changeCheckedStatus() {
 
